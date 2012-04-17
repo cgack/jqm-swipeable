682e06128ba732e0e0186ffb7d7519abe68ecbce
cgack <cory.gack@gmail.com>
2012-04-17 17:51:05 -0400

    prettify the js a bit

--- swipeable.js
+++ swipeable.js
@@ -23,25 +23,25 @@ $.widget( "cgack.swipeable", $.mobile.widget, {
 		var $el = this.element,
 			o = this.options,
 			swipeable = $el,
-			swipeheader =$el.find( o.heading ).first(),
+			swipeheader = $el.find( o.heading ).first(),
 			swipecontent = swipeable.wrapInner( "<div class='ui-swipeable-content' ></div>" ).find( ".ui-swipeable-content" );
 			
 		swipeable.addClass( "ui-swipeable" );
 		swipeheader.addClass( "ui-swipeable-header" ).insertBefore( swipecontent );
 		
-		o.showRight = swipeable.find(o.showRight).length > 0 ? swipeable.find(o.showRight).insertBefore( swipecontent ) : swipecontent;
-		o.showLeft = swipeable.find(o.showLeft).length > 0 ? swipeable.find(o.showLeft).insertBefore( swipecontent ) : swipecontent;
+		o.showRight = swipeable.find( o.showRight ).length > 0 ? swipeable.find( o.showRight ).insertBefore( swipecontent ) : swipecontent;
+		o.showLeft = swipeable.find( o.showLeft ).length > 0 ? swipeable.find( o.showLeft ).insertBefore( swipecontent ) : swipecontent;
 		
 		o.showRight.addClass( "ui-swipeable-rcontent" ).hide();
 		o.showLeft.addClass( "ui-swipeable-lcontent" ).hide();
 		//Determine the direction we want to bind our listeners to
-		var swiperight = swipeable.is(o.swipeRight) || o.swipeRight === true,
-				swipeleft = swipeable.is(o.swipeLeft) || o.swipeLeft === true,
-				swipeboth = swipeable.is(o.swipeBoth) || o.swipeBoth === true;
+		var swiperight = swipeable.is( o.swipeRight ) || o.swipeRight === true,
+				swipeleft = swipeable.is( o.swipeLeft ) || o.swipeLeft === true,
+				swipeboth = swipeable.is( o.swipeBoth ) || o.swipeBoth === true;
 
 		//Custom event bindings
 		swipeable.bind( "showRight", function() {
-			if (o.showRight.is( ":visible" )) {
+			if ( o.showRight.is( ":visible" ) ) {
 				o.showRight.hide();
 				if ( !o.showLeft.is( ":visible" ) ) {
 					swipeheader.show();
@@ -53,7 +53,7 @@ $.widget( "cgack.swipeable", $.mobile.widget, {
 		});
 		
 		swipeable.bind( "showLeft", function() {
-			if (o.showLeft.is( ":visible" )) {
+			if ( o.showLeft.is( ":visible" ) ) {
 				o.showLeft.hide();
 				if ( !o.showRight.is( ":visible" ) ) {
 					swipeheader.show();
@@ -69,11 +69,11 @@ $.widget( "cgack.swipeable", $.mobile.widget, {
 			if ( (swiperight  || swipeboth) && o.showRight.is( ":visible" )) {
 				o.showRight.hide()
 				ishidden = true;
-			} else if (!swipeleft){
+			} else if ( !swipeleft ){
 				o.showRight.show();
 			}
 
-			if ((swipeleft || swipeboth) && o.showLeft.is( ":visible" )) {
+			if ( (swipeleft || swipeboth) && o.showLeft.is( ":visible" ) ) {
 				o.showLeft.hide();
 				ishidden = true;
 			} else { 
@@ -88,16 +88,31 @@ $.widget( "cgack.swipeable", $.mobile.widget, {
 		});
 		//Bind our listeners fallback to click where there is no touch support
 		if ( swiperight ) {
-			swipeable.bind( "swiperight", function( event ) { swipeable.trigger( "showRight" ); event.preventDefault(); } );
+			swipeable.bind( "swiperight", function( event ) { 
+				swipeable.trigger( "showRight" ); 
+				event.preventDefault(); 
+			});
 		} else if ( swipeleft ) {
-			swipeable.bind( "swipeleft", function( event ) { swipeable.trigger( "showLeft" ); event.preventDefault(); } );
+			swipeable.bind( "swipeleft", function( event ) { 
+				swipeable.trigger( "showLeft" ); 
+				event.preventDefault(); 
+			});
 		}
 		if ( swipeboth ) {
-			swipeable.bind( "swipeleft", function( event) { swipeable.trigger( "showLeft" ); event.preventDefault(); });
-			swipeable.bind( "swiperight", function( event){ swipeable.trigger( "showRight" ); event.preventDefault(); });
+			swipeable.bind( "swipeleft", function( event) { 
+				swipeable.trigger( "showLeft" ); 
+				event.preventDefault(); 
+			});
+			swipeable.bind( "swiperight", function( event){ 
+				swipeable.trigger( "showRight" ); 
+				event.preventDefault(); 
+			});
 		} 
 	  if ( !$.support.touch ) {
-			swipeable.bind( "click", function( event ) { swipeable.trigger( "noswipe" ); event.preventDefault(); } );
+			swipeable.bind( "click", function( event ) { 
+				swipeable.trigger( "noswipe" ); 
+				event.preventDefault(); 
+			});
 		}
 	}
 });
